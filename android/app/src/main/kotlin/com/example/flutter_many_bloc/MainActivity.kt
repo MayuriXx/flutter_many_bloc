49493fb1a77aeeb77cb3dbb79d590d3f911d560f
Evan Martho <martho.evan@gmail.com>
2020-12-17 12:15:07 +0100

    chore : mise en place du projet

---
+++ android/app/src/main/kotlin/com/example/flutter_many_bloc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_many_bloc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_many_bloc/bloc/ui/main_scaffold.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Many Bloc',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MainScaffold(title: "Many BLoC",),
+    );
+  }
+}
