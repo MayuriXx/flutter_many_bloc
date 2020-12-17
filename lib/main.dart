f93fbc5cdcea9d10e69d6351c02771304c390619
Evan Martho <martho.evan@gmail.com>
2020-12-17 12:21:09 +0100

    fix - ui package déplacé

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_many_bloc/bloc/ui/main_scaffold.dart';
+import 'package:flutter_many_bloc/ui/main_scaffold.dart';
 
 void main() {
   runApp(MyApp());
